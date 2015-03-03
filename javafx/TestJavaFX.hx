3380993d1ecb0c00eca2fe001d49c41dca42c013
Cauê Waneck <waneck@gmail.com>
2015-03-03 07:02:39 -0300

    first commit: testing javafx

---
+++ javafx/TestJavaFX.hx
@@ -0,0 +1,31 @@
+// some typical imports taken from an example.
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.animation.TimelineBuilder;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import javafx.animation.Animation;
+import javafx.scene.Node;
+import javafx.application.Application;
+import javafx.scene.control.ButtonBuilder;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBoxBuilder;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.VBoxBuilder;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
+class TestJavaFX {
+
+    public static function main() { new TestJavaFX(); } public function new() {
+        trace('test');
+    }
+}
